07613b37e4b88b513cb41905964a3201ddd57662
biscotty666 <biscotty666@gmail.com>
2024-11-22 02:57:28 -0700

    Added chapter 2 greplite;

---
+++ greplite/src/main.rs
@@ -0,0 +1,53 @@
+use std::fs::File;
+use std::io;
+use std::io::BufReader;
+use std::io::prelude::*;
+use regex::Regex;
+use clap::{App,Arg};
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some (_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
+
+fn main() {
+    let args = App::new("greplite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(Arg::with_name("pattern")
+            .help("File to search for")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("input")
+            .help("File to search")
+            .takes_value(true)
+            .required(false))
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let input = args.value_of("input").unwrap_or("-");
+    let re = Regex::new(pattern).unwrap();
+
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
+    }
+
+}
+
+/*
+✦ ❯ cargo run -- picture input.txt
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.02s
+     Running `target/debug/greplite picture input.txt`
+dark square is a picture feverishly turned--in search of what?
+*/
